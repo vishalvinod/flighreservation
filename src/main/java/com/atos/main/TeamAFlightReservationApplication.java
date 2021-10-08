792d17d33a96ef7b3c6cba9876e73e2bd7b7ccb3
vishalvinod <vishalvinodkoduvally@gmail.com>
2021-10-08 10:52:53 +0530

    template added

---
+++ src/main/java/com/atos/main/TeamAFlightReservationApplication.java
@@ -0,0 +1,13 @@
+package com.atos.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamAFlightReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamAFlightReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atos/main/TeamAFlightReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atos.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamAFlightReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
